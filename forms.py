658ba216a6901cf64068f12b65b7889a091deb81
Josh <111018742+jhanna90@users.noreply.github.com>
2023-06-12 16:02:59 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,34 +1,3 @@
-# """Forms for playlist app."""
-
-# # from wsgiref.validate import validator
-# from wtforms import SelectField, StringField
-# from flask_wtf import FlaskForm
-# from wtforms.validators import InputRequired
-
-
-# class PlaylistForm(FlaskForm):
-#     """Form for adding playlists."""
-
-#     # Add the necessary code to use this form
-#     name = StringField("Name", validators=[InputRequired()])
-#     description = StringField("Description", validators=[InputRequired()])
-
-
-
-# class SongForm(FlaskForm):
-#     """Form for adding songs."""
-
-#     # Add the necessary code to use this form
-#     title = StringField("Title", validators=[InputRequired()])
-#     artist = StringField("Artist", validators=[InputRequired()])
-
-
-# # DO NOT MODIFY THIS FORM - EVERYTHING YOU NEED IS HERE
-# class NewSongForPlaylistForm(FlaskForm):
-#     """Form for adding a song to playlist."""
-
-#     song = SelectField('Song To Add', coerce=int)
-
 """Forms for playlist app."""
 
 from wtforms import validators, StringField, SelectField, TextAreaField
